491e4801853d73345fb722f0d31c820e3bbe5f3f
Eduardaskuliesa <eduardas2000@mail.ru>
2025-10-21 04:47:27 +0300

    chore: Returned auto languge suggestion

--- src/app/[locale]/(platform)/layout.tsx
+++ src/app/[locale]/(platform)/layout.tsx
@@ -1,5 +1,6 @@
 import CartSync from "./components/CartSync";
 import Footer from "./components/Footer";
+import LanguageSuggestion from "./components/LanguageSuggestion";
 import PlatformLoader from "./components/PlatformLoader";
 import ResponsiveNavBar from "./components/ResponsiveNavBar";
 import SyncUserPreferences from "./components/SyncUserPreferences";
@@ -15,6 +16,7 @@ export default function PlatformLayout({
       <main className="bg-gray-50 min-h-[80vh]">
         <CartSync />
         <SyncUserPreferences />
+        <LanguageSuggestion />
         <PlatformLoader>{children}</PlatformLoader>
       </main>
       <Footer />
